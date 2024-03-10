2bdf4389872d4433bd4fe0c547c4b780e208f8d2
HrithikD9 <hrithikdevnath0924@gmail.com>
2024-03-10 23:15:39 +0600

    first commit

---
+++ candiesANDtwoSister.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+#define      HeHe     return 0
+#define      YES      cout << "YES\n"
+#define      NO       cout << "NO\n"
+#define      nl         '\n'
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        ll n;
+        cin >> n;
+
+        cout << (n - 1) / 2 << nl;
+
+    }
+    HeHe;
+}
